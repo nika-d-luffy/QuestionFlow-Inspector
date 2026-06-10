fa0c61ba4a219be9f1f782d7c0b7d3c54ba196de
nika-d-luffy <luffydnika001@gmail.com>
2026-06-10 12:39:40 +0530

    Delete QuestionFlow Inspector/QuestionFlow Inspector directory

--- QuestionFlow Inspector/QuestionFlow Inspector/background.js
+++
@@ -1,8 +0,0 @@
-// background.js
-// Relays messages from content script to the overlay within the same tab.
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.type === "JSON_CAPTURED" && sender.tab?.id) {
-    chrome.tabs.sendMessage(sender.tab.id, message);
-  }
-});
